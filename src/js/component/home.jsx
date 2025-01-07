055bc5b5d31a3dc1100f3a3d3d553c4275eb27c7
Mar 🌊 <mariajara929@gmail.com>
2025-01-07 20:26:35 +0000

    keep working on it

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,36 +3,40 @@ import { ToDoInput } from "./toDoInput";
 
 const Home = () => {
 	const [toDoList, setToDoList] = useState([])
-	function deleteToDo(indexToBeDeleted) {
-		setToDoList((currentToDoList) => {
-			const newToDoList = currentToDoList.filter((_, index) => index != indexToBeDeleted)
-			return newToDoList
+
+
+	const deleteToDo = async (todoId) => {
+		let response = await fetch('https://playground.4geeks.com/todo/todos/' + todoId, {
+			method: "DELETE",
+
 		})
+		if (response.status == 204) {
+			getToDoList()
+		}
+		else {
+			console.log("error:", response.status, response.statusText)
+		}
+
+
 	}
-	function addToDo(todo) {
-		fetch('https://playground.4geeks.com/todo/todos/mariajara929', {
+	const addToDo = async (todo) => {
+		let response = await fetch('https://playground.4geeks.com/todo/todos/mariajara929', {
 			method: "POST",
 			body: JSON.stringify(todo),
 			headers: {
 				"Content-Type": "application/json"
 			}
 		})
-			.then(resp => {
-				console.log(resp.ok); // Will be true if the response is successful
-				if (!resp.ok) { throw new Error() }
-				console.log(resp.status); // The status code=200 or code=400 etc.
-				console.log(resp.text()); // Will try to return the exact result as a string
-				return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
-			})
-			.then(data => {
-				// Here is where your code should start after the fetch finishes
-				console.log(data); // This will print on the console the exact object received from the server
-				getToDoList()
-			})
-			.catch(error => {
-				// Error handling
-				console.error(error);
-			});
+		if (response.status == 201) {
+			let data = await response.json()
+			console.log(data)
+			getToDoList()
+		}
+		else {
+			console.log("error:", response.status, response.statusText)
+		}
+
+
 	}
 	function createUser() {
 		fetch('https://playground.4geeks.com/todo/users/mariajara929', {
@@ -56,30 +60,24 @@ const Home = () => {
 				console.error(error);
 			});
 	}
-	function getToDoList() {
-		fetch('https://playground.4geeks.com/todo/users/mariajara929', {
+	const getToDoList = async () => {
+
+		let response = await fetch('https://playground.4geeks.com/todo/users/mariajara929', {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json"
 			}
 		})
-			.then(resp => {
-				console.log(resp.ok); // Will be true if the response is successful
-				if (!resp.ok) {
-					createUser()
-				}
-				console.log(resp.status); // The status code=200 or code=400 etc.
-				return resp.json(); // (returns promise) Will try to parse the result as JSON and return a promise that you can .then for results
-			})
-			.then(data => {
-				// Here is where your code should start after the fetch finishes
-				console.log(data); // This will print on the console the exact object received from the server
-				setToDoList(data.todos)
-			})
-			.catch(error => {
-				// Error handling
-				console.error(error);
-			});
+
+		if (response.status == 404) {
+			createUser()
+		} else if (response.status == 200) {
+			let data = await response.json()
+			setToDoList(data.todos)
+		} else {
+			alert("There seems to be an issue retrieving your todos at the moment, please try again later...")
+		}
+
 	}
 	useEffect(() => {
 		getToDoList()
@@ -90,7 +88,7 @@ const Home = () => {
 			<ToDoInput toDoList={toDoList} setToDoList={setToDoList} addToDo={addToDo} />
 			{toDoList.map((value, index) => <div className="alert alert-dark col-6 d-flex justify-content-between">
 				<p>	{value.label} </p>
-				<span onClick={() => deleteToDo(index)} className="deleteButton btn btn-secondary"> X </span>
+				<span onClick={() => deleteToDo(value.id)} className="deleteButton btn btn-secondary"> X </span>
 			</div>)}
 		</div>
 	);
